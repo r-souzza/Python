c1c9760bc1d3f54c41864658224de0cc304d9267
Roger <rsouzza.2016@gmail.com>
2023-08-17 14:01:20 -0300

    Completando o código exercicio_6.py

--- Exercicios/Estruturas_Decisao/exercicio_6.py
+++ Exercicios/Estruturas_Decisao/exercicio_6.py
@@ -6,3 +6,9 @@ num3 = int(input("Digite outro número: "))
 
 if num1 > num2 and num1>num3:
     print(f'O número {num1} é o maior.')
+
+if num2 > num1 and num2 > num3:
+    print(f'O número {num2} é o maior.')
+
+if num3 > num1 and num3 > num2:
+    print(f'O número {num3} é o maior.')
