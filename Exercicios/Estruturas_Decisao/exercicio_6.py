c50ee1d416d8b7485d5e759e9f72ff9b97c4a39f
Roger <rsouzza.2016@gmail.com>
2023-08-17 13:50:17 -0300

    Adicionando exercicio_6.py em estruturas_Decisao

---
+++ Exercicios/Estruturas_Decisao/exercicio_6.py
@@ -0,0 +1,8 @@
+# Faça um Programa que leia três números e mostre o maior deles.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+num3 = int(input("Digite outro número: "))
+
+if num1 > num2 and num1>num3:
+    print(f'O número {num1} é o maior.')
