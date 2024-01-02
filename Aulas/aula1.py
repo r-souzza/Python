b805bc1079bcf8f5ab788d96d152e3ed93c134bc
Roger <rsouzza.2016@gmail.com>
2024-01-02 10:37:08 -0300

    Add dir Aulas

---
+++ Aulas/aula1.py
@@ -0,0 +1,15 @@
+# AULA 1: Aula sobre comentários e docstrings
+
+print('Hello World!')
+
+
+"""
+Exemplo de Docstring
+
+"""
+
+
+'''
+Exemplo de Docstring
+
+'''
---
+++ Aulas/aula2.py
@@ -0,0 +1,4 @@
+# AULA 2: Função print
+
+print(12, 34, sep='/') # sep - define o separador, o que fica entre os argumentos
+print(12, 34, end='\n') # end - define o final de linha
