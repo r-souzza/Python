08e33a030f9f698cb7d7800bf19950ab0b3623a3
Roger <rsouzza.2016@gmail.com>
2024-01-08 17:58:52 -0300

    Atualizando o conteúdo das aulas

--- Aulas/aula10.py
+++ Aulas/aula10.py
@@ -1,2 +1,10 @@
 # AULA 10: Aula sobre concatenação
 
+concatenacao = 'A' + 'B' + 'C' + str(1)
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_nome = 'Nome'
+
+print(a_dez_vezes)
+print(tres_vezes_nome)
---
+++ Aulas/aula11.py
@@ -0,0 +1,9 @@
+# AULA 11: Aula sobre precedência entre os operadores aritméticos (prioridade de cada operador)
+
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + - 
+
+conta_1 = 1 + 1 ** 5 + 5
+print(conta_1)
