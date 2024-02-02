cbeb7833d3b4aade7e83239405c0b212d7b2fff3
Roger <rsouzza.2016@gmail.com>
2024-02-02 17:21:55 -0300

    Atualizando o conteúdo das aulas

--- Aulas/aula28.py
+++ Aulas/aula28.py
@@ -17,17 +17,25 @@ Se nada for digitado em nome ou idade:
 
 """
 
-
 nome = input('Digite seu nome: ')
-nome_invertido = (nome[::-1])
-n = (len(nome))
-primeira_letra = (len(nome[0]))
-#idade = input('Digite sua idade: ')
+idade = input('Digite sua idade: ')
 
 
-print(f'Seu nome é {nome}')
-print(f'Seu nome invertido é {nome_invertido}')
-print(f'Seu nome tem {n} letras')
-print(f'A primeira letra do seu nome é {primeira_letra}')
 
+if nome and idade:
+
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+
+    if ' ' in nome:
+        print('Seu nome contém espaços.')
+    else:
+        print('Seu nome não contém espaços')
+
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+
+else:
+    print("Desculpe, você deixou campos vazios")
 
---
+++ Aulas/aula30.py
@@ -0,0 +1,17 @@
+# AULA 30: Variáveis, constantes e complexidade do código
+
+"""
+CONSTANTE = 'Variáveis' que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade, blocos dentro de outros blocos (ruim)
+
+"""
+
+velocidade = 61 # velocidade atual do carro
+local_carro = 101 # local em que o carro está na estrada
+
+# Costuma utilizar apenas letras maiusculas em CONSTANTES
+
+RADAR_1 = 60 # velocidade máxima do radar 1
+LOCAL_1 = 100 # local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
