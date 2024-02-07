869c1c16dd6e04dd3716f5875ee13ed4fb86d20a
Roger <rsouzza.2016@gmail.com>
2024-02-07 14:19:39 -0300

    Atualizando o conteúdo das aulas

--- Aulas/aula30.py
+++ Aulas/aula30.py
@@ -7,11 +7,24 @@ Muitas condições no mesmo if (ruim)
 
 """
 
-velocidade = 61 # velocidade atual do carro
+velocidade = 62 # velocidade atual do carro
 local_carro = 101 # local em que o carro está na estrada
 
 # Costuma utilizar apenas letras maiusculas em CONSTANTES
 
-RADAR_1 = 60 # velocidade máxima do radar 1
+RADAR_1 = 61 # velocidade máxima do radar 1
 LOCAL_1 = 100 # local onde o radar 1 está
 RADAR_RANGE = 1 # A distância onde o radar pega
+
+vel_car_pass_radar_1 = velocidade > RADAR_1
+
+carro_multado_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGE) and \
+        velocidade > RADAR_1
+
+if vel_car_pass_radar_1:
+    print('Velocidade do carro acima do radar 1')
+    
+
+if carro_multado_radar_1 and vel_car_pass_radar_1:
+        print("Carro multado em radar 1")
