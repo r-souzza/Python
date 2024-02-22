71c7e82943b894e5f9c657878e0727bc49f9646d
Roger <rsouzza.2016@gmail.com>
2024-02-22 17:14:28 -0300

    Atualizando o conteúdo das aulas

---
+++ Aulas/aula36.py
@@ -0,0 +1,16 @@
+# AULA 36: Operadores de atribuição com operadores aritméticos
+
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+
+"""
+
+contador = 0
+
+
+
+contador = contador + 1 # ou contador += 1
+#contador *= 2
+# OBS: funciona também com strings
+print(contador)
